bb6892acb026073b00eae4eb0f1fd21ad3ba5a93
イワシの匠 <44769766+ZEEK-ON@users.noreply.github.com>
2023-11-15 15:46:09 +0000

    Update 7 files

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,14 @@ import "./styles.css";
 import { useState } from "react";
 import { Todo } from "./Todo";
 import { TodoType } from "./types/todo";
+import { Text } from "./Text";
+import { UserProfile } from "./UserProfile";
+import { User } from "./types/user";
+
+const user: User = {
+  name: "イワシ",
+  hobbies: ["映画", "ゲーム"],
+};
 
 export default function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
@@ -17,10 +25,13 @@ export default function App() {
   };
   return (
     <div className="App">
+      <UserProfile user={user} />
+      <Text color="red" fontSize="18px" />
       <button onClick={onCLickFetchData}>データ取得</button>
       {todos.map((todo) => (
         // 型がないとエラーも無く気づけない
         <Todo
+          // id={0}
           key={todo.id}
           title={todo.title}
           userId={todo.userId}
--- src/Todo.tsx
+++ src/Todo.tsx
@@ -8,7 +8,11 @@ import { TodoType } from "./types/todo";
 // };
 
 // export const Todo = (props: { title: any; userid: any; completed: any; }) => {
-export const Todo = (props: TodoType) => {
+// export const Todo = (props: TodoType) => {
+export const Todo = (
+  // props: Pick<TodoType, "userId" | "title" | "completed">,
+  props: Omit<TodoType, "id">,
+) => {
   const { title, userId, completed = false } = props;
   const completeMark = completed ? "[完]" : "[未]";
   return <p>{`${completeMark} ${title}(ユーザー:${userId})`}</p>;
